950ebb1a84df49b9462631a0b2c5ce654cbf2152
Alan Rocha <141082245+alanrochagomes@users.noreply.github.com>
2024-02-02 11:10:39 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil_jornada_android_fevereiro_2024/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil_jornada_android_fevereiro_2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
